c220858f44b4f3436af886b14bd550b5fc552c88
Prashant <146057974+prashantsorout001@users.noreply.github.com>
2025-09-26 18:07:43 +0530

    Add files via upload

---
+++ interface/EmployeeManagement.java
@@ -0,0 +1,72 @@
+class Employee {
+    String name;
+    int id;
+    double salary;
+
+    public Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    public void displayDetails() {
+        System.out.println("ID: " + id + ", Name: " + name + ", Salary: " + salary);
+    }
+}
+
+class Manager extends Employee {
+    int teamSize;
+
+    public Manager(String name, int id, double salary, int teamSize) {
+        super(name, id, salary);
+        this.teamSize = teamSize;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Team Size: " + teamSize);
+    }
+}
+
+class Developer extends Employee {
+    String programmingLanguage;
+
+    public Developer(String name, int id, double salary, String programmingLanguage) {
+        super(name, id, salary);
+        this.programmingLanguage = programmingLanguage;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Programming Language: " + programmingLanguage);
+    }
+}
+
+class Intern extends Employee {
+    int duration;
+
+    public Intern(String name, int id, double salary, int duration) {
+        super(name, id, salary);
+        this.duration = duration;
+    }
+
+    @Override
+    public void displayDetails() {
+        super.displayDetails();
+        System.out.println("Internship Duration: " + duration + " months");
+    }
+}
+
+public class EmployeeManagement {
+    public static void main(String[] args) {
+        Manager m = new Manager("Raj", 101, 75000, 5);
+        Developer d = new Developer("Aditya", 102, 50000, "Java");
+        Intern i = new Intern("Nitin", 103, 10000, 3);
+
+        m.displayDetails();
+        d.displayDetails();
+        i.displayDetails();
+    }
+}
---
+++ interface/VehicleTransportSystem.java
@@ -0,0 +1,72 @@
+class Vehicle {
+    int maxSpeed;
+    String fuelType;
+
+    public Vehicle(int maxSpeed, String fuelType) {
+        this.maxSpeed = maxSpeed;
+        this.fuelType = fuelType;
+    }
+
+    public void displayInfo() {
+        System.out.println("Max Speed: " + maxSpeed + " Fuel Type: " + fuelType);
+    }
+}
+
+class Car extends Vehicle {
+    int seatCapacity;
+
+    public Car(int maxSpeed, String fuelType, int seatCapacity) {
+        super(maxSpeed, fuelType);
+        this.seatCapacity = seatCapacity;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Seat Capacity: " + seatCapacity);
+    }
+}
+
+class Truck extends Vehicle {
+    int loadCapacity;
+
+    public Truck(int maxSpeed, String fuelType, int loadCapacity) {
+        super(maxSpeed, fuelType);
+        this.loadCapacity = loadCapacity;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Load Capacity: " + loadCapacity + " tons");
+    }
+}
+
+class Motorcycle extends Vehicle {
+    boolean hasCarrier;
+
+    public Motorcycle(int maxSpeed, String fuelType, boolean hasCarrier) {
+        super(maxSpeed, fuelType);
+        this.hasCarrier = hasCarrier;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Has Carrier: " + hasCarrier);
+    }
+}
+
+public class VehicleTransportSystem {
+    public static void main(String[] args) {
+        Vehicle[] vehicles = new Vehicle[3];
+        vehicles[0] = new Car(180, "Petrol", 5);
+        vehicles[1] = new Truck(120, "Diesel", 10);
+        vehicles[2] = new Motorcycle(100, "Petrol", true);
+
+        for (Vehicle v : vehicles) {
+            v.displayInfo();
+            System.out.println("----------------");
+        }
+    }
+}
